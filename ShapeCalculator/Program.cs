900763956da9dd17675c8b8c8f40cf51895e6b8d
folka2134 <folka2134@gmail.com>
2025-01-29 14:42:10 +0000

    feature: program loop

--- ShapeCalculator/Program.cs
+++ ShapeCalculator/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace ShapeCalculator
+﻿using ShapeCalculator.Controllers;
+using ShapeCalculator.Services;
+using ShapeCalculator.Views;
+
+namespace ShapeCalculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            var view = new ConsoleView();
+            var calculator = new Calculator();
+            var controller = new CalculatorController(view, calculator);
+            controller.Run();
         }
     }
 }
